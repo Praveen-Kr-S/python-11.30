3835831cc2004782907509566296b75d87091279
PraveenKumar S <praveenkrsslm69@gmail.com>
2026-02-23 15:44:37 +0530

    Add files via upload

---
+++ demo_modules.py
@@ -0,0 +1,70 @@
+#Adavnced modules
+
+# import math
+# print(math.pi)
+# print(math.factorial(5))
+# print(math.pow(5,3))
+
+# import pywhatkit as pk
+# import time
+# import pyautogui as pg
+
+# pk.search("Vijay TVK")
+# pk.playonyt("claude.ai")
+
+
+# pk.sendwhatmsg_instantly("+91 8838260257", "Hello Stephen \nI am Python")
+# time.sleep(5)
+# pg.doubleClick()
+
+
+
+
+#pygame
+# import pygame
+# import time
+# pygame.init()
+# pygame.display.set_caption("Free Fire")
+# ps = pygame.display.set_mode((900,600))
+# ps1 = pygame.image.load(r"C:\Users\rajir\OneDrive\Pictures\fg.jpg")
+# ps = ps.blit(ps1,(10,10))
+# pygame.display.update()
+# time.sleep(5)
+# pygame.quit()
+
+# from PIL import Image
+# img = Image.open(r"C:\Users\rajir\OneDrive\Pictures\dhoni.jpg")
+# img = img.resize((400,700))
+# img.show()
+
+#scipy
+from scipy import constants as si
+# print(si.proton_mass)
+# print(si.hour)
+# print(si.g)
+# print(si.bar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
